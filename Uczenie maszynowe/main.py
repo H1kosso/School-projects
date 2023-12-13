5c6399b6a8a4840a757672b22e4bc18b53b3ab84
H1kosso <marcin.hikosso@gmail.com>
2023-12-13 23:00:11 +0100

    Machine Learning fix

--- Uczenie maszynowe/main.py
+++ Uczenie maszynowe/main.py
@@ -28,36 +28,44 @@ X_train, X_test, y_train, y_test = train_test_split(df['text'], df['class'],
 
 classifiers = [
     ('Decision Tree', DecisionTreeClassifier()),
-    ('Random Forest', RandomForestClassifier(n_estimators=100,
-                                             random_state=42)),
+    ('Random Forest', RandomForestClassifier(n_estimators=100, random_state=42)),
     ('SVM', SVC())
 ]
+
+vectorizers = [
+    ('CountVectorizer_3', CountVectorizer(token_pattern=r'\b\w{3,}\b')),
+    ('CountVectorizer_4', CountVectorizer(token_pattern=r'\b\w{4,}\b')),
+    ('CountVectorizer_5', CountVectorizer(token_pattern=r'\b\w{5,}\b'))
+]
+
 results = []
-for classifier_name, classifier in classifiers:
-    # Utwórz pipeline z CountVectorizer i klasyfikatorem
-    pipeline = Pipeline([('vectorizer', CountVectorizer(token_pattern=r'\b\w{3,}\b')), ('classifier', classifier)
-                         ])
-    cv_scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring='accuracy')
+for vectorizer_name, vectorizer in vectorizers:
+    for classifier_name, classifier in classifiers:
+        # Utwórz pipeline z CountVectorizer i klasyfikatorem
+        pipeline = Pipeline([('vectorizer', vectorizer), ('classifier', classifier)])
+        cv_scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring='accuracy')
 
-    # Trenuj model
-    pipeline.fit(X_train, y_train)
+        # Trenuj model
+        pipeline.fit(X_train, y_train)
 
-    # Przewiduj na danych testowych
-    y_pred = pipeline.predict(X_test)
+        # Przewiduj na danych testowych
+        y_pred = pipeline.predict(X_test)
 
-    # Oceniaj wyniki
-    accuracy = accuracy_score(y_test, y_pred)
-    report = classification_report(y_test, y_pred)
+        # Oceniaj wyniki
+        accuracy = accuracy_score(y_test, y_pred)
+        report = classification_report(y_test, y_pred)
 
-    # Dodaj wyniki do listy
-    results.append({
-        'Classifier': classifier_name,
-        'Mean Accuracy': cv_scores.mean(),
-        'Cross-Validation Scores': cv_scores,
-        'Classification Report': report
-    })
+        # Dodaj wyniki do listy
+        results.append({
+            'Vectorizer': vectorizer_name,
+            'Classifier': classifier_name,
+            'Mean Accuracy': cv_scores.mean(),
+            'Cross-Validation Scores': cv_scores,
+            'Classification Report': report
+        })
 
 for result in results:
+    print(f"Vectorizer: {result['Vectorizer']}")
     print(f"Classifier: {result['Classifier']}")
     print(f"Cross-Validation Scores: {result['Cross-Validation Scores']}")
     print(f"Mean CV Accuracy: {result['Mean Accuracy']:.4f}")
